81337f3ad2a64703743ae80bc01106b6c94f6693
Ryan <ryan.perz@gmail.com>
2012-10-01 14:41:16 -0500

    first commit of new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chico::Application.config.secret_token = '8fdb11225e9da48daf24607f5459961525c9c6757825153c28f48ebff29c0d544b9b24778cf5ef9dd26ac0b2a75e73cc978dce15689483934f899b799d34c472'

d51ddef991f0386786a2412de736a0bf3111b320
Ryan <ryan.perz@gmail.com>
2014-03-19 21:43:31 -0500

    trying to get twitter authorization to work

--- app/controllers/application_controller.rb~
+++ app/controllers/application_controller.rb~
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
  # end
 
 def current_user=(user)
-  @current_user = user
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]  
   session[:user_id] = user.id
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,13 @@ class User < ActiveRecord::Base
     create(:name => hash['info']['name'])
   end
 
-
+def self.create_with_omniauth(auth)  
+    create! do |user|  
+      user.provider = auth["provider"]  
+      user.uid = auth["uid"]  
+      user.name = auth["user_info"]["name"]  
+    end  
+  end  
 
 
 end

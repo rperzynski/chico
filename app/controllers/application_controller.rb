ccee19a3d165b976abee440fbed1553422bb456e
Karl Stolley <karl.stolley@gmail.com>
2014-04-29 13:15:19 -0500

    Clean up the source on Application Controller and reintroduce the protect_from_forgery method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+
   protected
 
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 
-
-def current_user
-   @current_user ||= User.find_by_id(session[:user_id])
-end
-
-def current_user=(user)
-  @current_user = user
-  session[:user_id] = user.id
-end
+  def current_user=(user)
+    @current_user = user
+    # Set some session variables
+    session[:user_id] = user.id
+    session[:name] = user.name
+  end
 
 end
